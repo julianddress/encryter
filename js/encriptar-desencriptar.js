0c5acf82ace5a3d8f67fa584920bd675d5e4cad4
julianddress <andres.mtta1@gmil.com>
2023-05-10 15:00:28 -0500

    ADDING FUNCTIONS FOR INDEX.HTML FILE IN JAVASCRIPT

---
+++ js/encriptar-desencriptar.js
@@ -0,0 +1,18 @@
+// FUNCTION - ENCRIPTAR TEXTO
+const encriptarTexto = () =>{
+
+}
+
+// -----------------------------------------------------------------------------------------            
+
+// FUNCTION - DESENCRIPTAR TEXTO
+const desencriptarTexto = () =>{
+
+}
+
+// -----------------------------------------------------------------------------------------            
+
+// FUNCTION - COPY TEXT
+const copiarTexto = () => {
+
+}
