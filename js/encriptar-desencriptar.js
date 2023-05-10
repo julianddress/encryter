8a3cf2bd5b52c36ff03ef6639d1b854a0253a170
julianddress <andres.mtta1@gmil.com>
2023-05-10 15:02:37 -0500

    CALLING THE FUNCTIONS THAT ALREADY HAS BEEN CREATED

--- js/encriptar-desencriptar.js
+++ js/encriptar-desencriptar.js
@@ -1,3 +1,6 @@
+let input1 = document.querySelector("#input-redactar");
+input1.focus();
+
 // FUNCTION - ENCRIPTAR TEXTO
 const encriptarTexto = () =>{
 
@@ -16,3 +19,12 @@ const desencriptarTexto = () =>{
 const copiarTexto = () => {
 
 }
+
+let desencriptar = document.querySelector("#boton-desencriptar");
+desencriptar.onclick = desencriptarTexto;
+
+let opcionCopiar = document.querySelector("#boton-copiar");
+opcionCopiar.onclick = copiarTexto;
+
+let encriptar = document.querySelector("#boton-encriptar");
+encriptar.onclick = encriptarTexto;
